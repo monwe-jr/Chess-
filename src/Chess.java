b8290ea1a9ee852b96efce7e6d5bb1f2eae68a35
Jashan <jashan.toronto@gmail.com>
2021-11-13 00:34:02 -0500

    Update Chess.java

--- src/Chess.java
+++ src/Chess.java
@@ -2,6 +2,10 @@ public class Chess {
 
  Chess(){
   System.out.println("hey");
+  for (int i=0; i<5; i++) {
+       System.out.println("test");
+  }
+
  }
 
  public static void main(String[] args) {
