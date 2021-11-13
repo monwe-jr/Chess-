5e1777473564f2e08cd063a2d0a28978c0df27e5
monwe-jr <monwefrancis@gmail.com>
2021-11-13 00:38:48 -0500

    Removed for loop

--- src/Chess.java
+++ src/Chess.java
@@ -2,10 +2,7 @@ public class Chess {
 
  Chess(){
   System.out.println("hey");
-  for (int i=0; i<5; i++) {
-       System.out.println("test");
-  }
-
+  
  }
 
  public static void main(String[] args) {
